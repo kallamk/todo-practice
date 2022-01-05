f702ebd519d0a0dbac16d6b673d2d1bbcebec930
kallamk <97184239+kallamk@users.noreply.github.com>
2022-01-05 22:33:41 +0530

    Update BasicAuthenticationController.java

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/BasicAuthenticationController.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/BasicAuthenticationController.java
@@ -3,7 +3,9 @@ package com.in28minutes.rest.webservices.restfulwebservices.helloworld;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+/**
 
+Basic authentication controller */
 // controller
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
